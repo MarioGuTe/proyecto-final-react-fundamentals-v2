55e4a602291e61ea3b000043fd004c581ab116c5
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-28 16:32:21 -0400

    add basic folder structure

---
+++ src/atoms/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import s from "./particle/style.module.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>Hello im app</div>;
+}
+
+export default App;
