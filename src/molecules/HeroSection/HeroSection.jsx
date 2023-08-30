a903ac2224feaa1071510ca7c241a53ca8bb982d
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-30 00:17:08 -0400

    add style and structure

---
+++ src/molecules/HeroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import s from "./particle/style.module.css";
+
+const HeroSection = () => {
+  return (
+    <div className={s.title_section}>
+      <h1>FantastiFotos</h1>
+      <p>fotografías profesionales a tu alcance</p>
+    </div>
+  );
+};
+
+export default HeroSection;
