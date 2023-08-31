2ccb475d02794beafb614469077970da1a705b6d
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-31 00:21:30 -0400

    add fetchImages and fetchByQuery

---
+++ src/api/pixabay-data.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { BASE_URL, API_KEY_PARAM } from "../config";
+
+export class PixabayApi {
+  static async fetchImages() {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}${API_KEY_PARAM}&image_type=photo&pretty=true`
+      );
+      // console.log(response.data.hits);
+      return response.data.hits;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  static async fetchByQuery(query) {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}${API_KEY_PARAM}&q=${query}`
+      );
+      // console.log(response.data.hits);
+      return response.data.hits;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
