ecf2043eec6aeb46ebb75d3dc22132c3f6e0e8b0
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-31 00:20:46 -0400

    update logic and style

--- src/molecules/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/molecules/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,18 +1,13 @@
 import GalleryItemLikes from "../GalleryItemLikes/GalleryItemLikes";
+import GalleryItemTags from "../GalleryItemTags/GalleryItemTags";
 import s from "./particle/style.module.css";
 
 const ImageGalleryItem = ({ image }) => {
-  const tags = image.tags.split(",");
-
   return (
     <div className={s.img_container}>
       <GalleryItemLikes likes={image} />
-      <div className={s.details_info_container}>
-        {tags.map((tag) => (
-          <p key={tag}>#{tag.trimStart()}</p>
-        ))}
-      </div>
-      <img src={image.webformatURL} alt="alt" />
+      <GalleryItemTags tagList={image} />
+      <img src={image.webformatURL} alt={image.tags} />
     </div>
   );
 };
