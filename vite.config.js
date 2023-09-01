4cf4b3a5192ad309a1c1ead5309c2dde7462fa8d
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-31 20:57:56 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 4270,
+  },
+  // build options
+  build: {
+    incremental: true,
+    babel: {
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    },
+
+    cache: true,
+    minify: true,
+    cssMinify: true,
+    mode: "production",
+    chunks: true,
+    moduleBundling: true,
+    prerenderPaths: ["/"],
+
+    modulePreload: true,
+    outDir: "build",
+  },
+});
